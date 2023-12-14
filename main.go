1be0c7dd2171b6b413e32ec25ee941290af59861
orme292 <andrew.orme@me.com>
2023-12-13 20:48:33 -0500

    Move TODO list

--- main.go
+++ main.go
@@ -11,6 +11,27 @@ import (
 	"github.com/orme292/s3packer/s3pack"
 )
 
+// TODO: Option to turn of checksum tagging (big bottleneck)
+// TODO: More debug messages
+// TODO: Concurrent checksum tagging
+// TODO: Remove FileObject attribute ShouldMultiPart, not used.
+// TODO: Overwrite options -- only-if checksum changes (overwrite: always, on-change, never)
+// TODO: Upload/Ignore function return args can be removed -- they can be counted on the fly
+// TODO: LogBot, support sprintf style formatting
+// TODO: Config, add naming section for KeyNamingMethod, pathPrefix, etc
+// TODO: Config, rename indexes from camel case to dashed "pathPrefix" to "path-prefix"
+// TODO: Add some console styling, maybe a progress bar.
+// TODO: Add silent option
+// TODO: Add option to create sample profile YAML
+// TODO: Update all comments for each function/method.
+// TODO: Update CHANGELOG.md
+// TODO: Update README.md
+// TODO: Update VERSION
+// TODO: Add more readable log output, check log levels make sense
+// TODO: Consider ErrorAs implementation and hard coding error messages in Const
+// TODO: Upgrade to AWS SDK v2
+// TODO: OCI support
+
 /*
 getFlags uses the flag package to configure and get command line arguments. It returns:
 -- profile: The filename of the profile to load.
--- s3pack/main.go
+++ s3pack/main.go
@@ -7,23 +7,6 @@ import (
 	"github.com/orme292/s3packer/config"
 )
 
-// TODO: Overwrite options -- only-if checksum changes (overwrite: always, on-change, never)
-// TODO: Upload/Ignore function return args can be removed -- they can be counted on the fly
-// TODO: LogBot, support sprintf style formatting
-// TODO: Config, add naming section for KeyNamingMethod, pathPrefix, etc
-// TODO: Config, rename indexes from camel case to dashed "pathPrefix" to "path-prefix"
-// TODO: Add some console styling, maybe a progress bar.
-// TODO: Add silent option
-// TODO: Add option to create sample profile YAML
-// TODO: Update all comments for each function/method.
-// TODO: Update CHANGELOG.md
-// TODO: Update README.md
-// TODO: Update VERSION
-// TODO: Add more readable log output, check log levels make sense
-// TODO: Consider ErrorAs implementation and hard coding error messages in Const
-// TODO: Upgrade to AWS SDK v2
-// TODO: OCI support
-
 func IndividualFileUploader(c *config.Configuration, files []string) (err error, uploaded, ignored int) {
 	exists, err := BucketExists(c)
 	if err != nil {
