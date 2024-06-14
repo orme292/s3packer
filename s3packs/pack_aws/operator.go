10c519459e8e978b206a27e609c483e366d522d9
Andrew Orme <orme292@users.noreply.github.com>
2024-06-14 16:48:20 -0400

    Update AWS SDK, support region issues with us-east-1 (#125)
    Signed-off-by: orme292 <orme292@users.noreply.github.com>

--- s3packs/pack_aws/operator.go
+++ s3packs/pack_aws/operator.go
@@ -25,11 +25,12 @@ func NewAwsOperator(ac *conf.AppConfig) (*AwsOperator, error) {
 
 func (op *AwsOperator) CreateBucket() (err error) {
 	input := &s3.CreateBucketInput{
-		Bucket: aws.String(op.ac.Bucket.Name),
-		ACL:    types.BucketCannedACL(op.ac.Provider.AWS.ACL),
-		CreateBucketConfiguration: &types.CreateBucketConfiguration{
-			LocationConstraint: types.BucketLocationConstraint(op.ac.Bucket.Region),
-		},
+		Bucket:                    aws.String(op.ac.Bucket.Name),
+		ACL:                       types.BucketCannedACL(op.ac.Provider.AWS.ACL),
+		CreateBucketConfiguration: &types.CreateBucketConfiguration{},
+	}
+	if op.ac.Bucket.Region != "us-east-1" {
+		input.CreateBucketConfiguration.LocationConstraint = types.BucketLocationConstraint(op.ac.Bucket.Region)
 	}
 	_, err = op.client.CreateBucket(context.Background(), input)
 	if err != nil {
