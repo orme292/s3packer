35e8ca52bbee8a2b95770526c076374bcb630301
orme292 <orme292@users.noreply.github.com>
2024-07-02 13:35:54 -0400

    Expand the Stats type
    Signed-off-by: orme292 <orme292@users.noreply.github.com>

--- s3packs/provider_v2/type_stats.go
+++ s3packs/provider_v2/type_stats.go
@@ -2,10 +2,23 @@ package provider_v2
 
 type Stats struct {
 	Objects int
-	Bytes   int64
+	Failed  int
+	Skipped int
+
+	Bytes int64
 }
 
-func (s *Stats) Increment(n int, b int64) {
+func (s *Stats) IncObjects(n int, b int64) {
 	s.Objects += n
+	s.Bytes += b
+}
+
+func (s *Stats) IncFailed(n int, b int64) {
+	s.Failed += n
+	s.Bytes += b
+}
+
+func (s *Stats) IncSkipped(n int, b int64) {
+	s.Skipped += n
 	s.Bytes += b
 }
